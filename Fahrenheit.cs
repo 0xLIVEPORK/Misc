1a71df52766dbf28adc393cf2d8a4ee595dafb4a
EVILPORK <0xlivepork@gmail.com>
2025-05-04 22:51:31 +0800

    Create Fahrenheit.cs

---
+++ Fahrenheit.cs
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/*
+ Variables and Arithmetic Expressions 
+ The next program uses the formula • 
+ C - (5/9)(• F-32) to print the follow- ing table of 
+ Fahrenheit temperatures and their centigrade or Celsius
+  equivalents: 
+  0 -17 
+  20 -6 
+  40 4 
+  60 15 
+  80 26 
+  100 37 
+  120 48 
+  140 60 
+  160 71 
+  180 82 
+  200 93 
+  220 104 
+  240 115 
+  260 126 
+  280 137 
+  300 148
+*/
+main()
+{
+    int fahrenheit = 0;
+    int celsius;
+
+    while(fahrenheit <= 300)
+    {
+        celsius = 5 * (fahrenheit-32) / 9;
+        printf("F: %d \t C: %d\n", fahrenheit, celsius);
+        fahrenheit += 20;
+    }
+}
