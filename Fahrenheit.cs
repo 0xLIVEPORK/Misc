f2702021ccae907706dd466495e2bf0212cfab9f
EVILPORK <0xlivepork@gmail.com>
2025-05-04 23:05:42 +0800

    Update Fahrenheit.cs

--- Fahrenheit.cs
+++ Fahrenheit.cs
@@ -29,8 +29,8 @@ int main(void)
 
     while(fahrenheit <= 300.0)
     {
-        celsius = (5.0 / 9.0) * (fahrenheit-32.0);
-        printf("F: %f \t C: %f\n", fahrenheit, celsius);
+        celsius = (5.0 / 9.0) * (fahrenheit - 32.0);
+        printf("F: %f \t C: %f\n", fahrenheit, celsius); //Let's not forget that for some goddamn reason K & R decided the %d is FOR INTEGERS AND NOT FOR FUCKING DOUBLES OR DECIMALS WHAT THE FUCK
         fahrenheit += 20.0;
     }
 }
