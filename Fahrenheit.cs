e1ec72d217b0b95e64c3306df4a3f28c51a6a487
EVILPORK <0xlivepork@gmail.com>
2025-05-04 23:04:55 +0800

    Update Fahrenheit.cs

--- Fahrenheit.cs
+++ Fahrenheit.cs
@@ -30,7 +30,7 @@ int main(void)
     while(fahrenheit <= 300.0)
     {
         celsius = (5.0 / 9.0) * (fahrenheit-32.0);
-        printf("F: %d \t C: %d\n", fahrenheit, celsius);
+        printf("F: %f \t C: %f\n", fahrenheit, celsius);
         fahrenheit += 20.0;
     }
 }
