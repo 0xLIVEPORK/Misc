dd72f1588dfb4557d1436ea69ae6a3b5c96e919d
EVILPORK <0xlivepork@gmail.com>
2025-05-04 23:04:42 +0800

    Update Fahrenheit.cs

--- Fahrenheit.cs
+++ Fahrenheit.cs
@@ -1,14 +1,36 @@
 #include <stdio.h>
-
-main()
+/*
+ Variables and Arithmetic Expressions 
+ The next program uses the formula • 
+ C - (5/9)(• F-32) to print the follow- ing table of 
+ Fahrenheit temperatures and their centigrade or Celsius
+  equivalents: 
+  0 -17 
+  20 -6 
+  40 4 
+  60 15 
+  80 26 
+  100 37 
+  120 48 
+  140 60 
+  160 71 
+  180 82 
+  200 93 
+  220 104 
+  240 115 
+  260 126 
+  280 137 
+  300 148
+*/
+int main(void)
 {
-    int fahrenheit = 0;
-    int celsius;
+    double fahrenheit = 0;
+    double celsius;
 
-    while(fahrenheit <= 300)
+    while(fahrenheit <= 300.0)
     {
-        celsius = 5 * (fahrenheit-32) / 9;
+        celsius = (5.0 / 9.0) * (fahrenheit-32.0);
         printf("F: %d \t C: %d\n", fahrenheit, celsius);
-        fahrenheit += 20;
+        fahrenheit += 20.0;
     }
 }
