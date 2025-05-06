f10f880905805d36e166bb643f90c88471f399cd
EVILPORK <0xlivepork@gmail.com>
2025-05-06 16:36:15 +0800

    Update BasicInputOutput.c

--- C Snippets/BasicInputOutput.c
+++ C Snippets/BasicInputOutput.c
@@ -11,3 +11,16 @@ main()
         character = getchar();
     }
 }
+
+//************************************
+//2ND VER
+
+#include <stdio.h>
+
+main()
+{
+    int character;
+
+    while((character = getchar()) != EOF)
+        putchar(character);
+}
