bb7286f3d8c6643c236281453ca23ac8765e5de9
EVILPORK <0xlivepork@gmail.com>
2025-05-06 20:36:52 +0800

    Update BasicInputOutput.c

--- C Snippets/BasicInputOutput.c
+++ C Snippets/BasicInputOutput.c
@@ -24,3 +24,21 @@ main()
     while((character = getchar()) != EOF)
         putchar(character);
 }
+
+//************************************
+//EXERCISE
+
+#include <stdio.h>
+//Exercise 1-6. Verify that the expression getchar ( ) I= EOF is 0 or 1. 
+//Exercise 1-7. Write a program to print the value of EOF. 
+
+main() {
+    int c;
+
+    while((c = getchar()) != EOF) 
+        putchar(c);
+    
+    printf("EXPRESSION VALUE: %d\n", c = getchar() != EOF);
+    printf("EOF VALUE: %d\n", EOF);
+
+}
