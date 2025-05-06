8157a2e6db0a88ae86118a9ec4eb2dce5601e90c
EVILPORK <0xlivepork@gmail.com>
2025-05-06 22:28:24 +0800

    Update CountingBasic.c

--- C Snippets/CountingBasic.c
+++ C Snippets/CountingBasic.c
@@ -9,3 +9,15 @@ main()
 
     printf("\n%ld", num_count);
 }
+
+//2ND VER
+
+#include <stdio.h> 
+
+main()
+{
+    long count1;
+    for(count1 = 0; getchar() != EOF; ++count1)
+    ;
+    printf("%ld\n", count1);
+}
