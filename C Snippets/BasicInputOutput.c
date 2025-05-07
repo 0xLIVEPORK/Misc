04a52a0735d738904f10f92e0611f25b00878c95
EVILPORK <0xlivepork@gmail.com>
2025-05-07 22:34:16 +0800

    Update BasicInputOutput.c

--- C Snippets/BasicInputOutput.c
+++ C Snippets/BasicInputOutput.c
@@ -1,4 +1,4 @@
-//THIS TO NOTE THAT I AM USING KERNIGHAM AND RITCHIE'S THE C PROGRAMMING LANGUAGE 2ND ED. AS REFERENCE
+e//THIS TO NOTE THAT I AM USING KERNIGHAM AND RITCHIE'S THE C PROGRAMMING LANGUAGE 2ND ED. AS REFERENCE
 //WHICH IS FROM PRE - C89 STANDARDS
 
 #include <stdio.h>
@@ -55,23 +55,4 @@ main() {
 #define OUT 0
 
 main()
-{
-    int c, lineCount, wordCount, charCount, STATE;
-
-    c = lineCount = wordCount = charCount, STATE = 0;
-
-    while((c = getchar()) != EOF)
-    {
-        if(c == '\n' || c == ' ' || c == '\t')
-            STATE = OUT;
-            lineCount + IN;
-        
-        if(c == '\n')
-            wordCount + IN;
-        charCount + IN;
-    }
-    printf("\n%d LINE NUMBER\n%d WORD NUMBER\n%d CHAR NUMBER", lineCount, wordCount, charCount);
-}
-
-
-
+1.5.4 Word Counting The fourth in our series of useful programs counts lines, words, and charac- ters, with the loose definition that a word is any sequence of characters that does not contain a blank, tab or newline. This is a bare-bones version of the UNIX program we. #include <stdio.h> #define IN 1 #define OUT 0 /* inside a word */ /* outside a word */ I* count lines, words, and characters in input */ main() { } int c, nl, nw, nc, state; state = OUT; nl = nw = nc = O; while ((c = getchar()) I= EOF) { ++nc; } if ( c == '\n' ) ++nl; if (c ==''II c == '\n' II c == '\t') state = OUT; else if (state == OUT) { state = IN; ++nw; } printf("%d %d %d\n", nl, nw, nc); Every time the program encounters the first character of a word, it counts SECTION 1.5 CHARACTER INPUT AND OUTPUT 21 one more word. The variable state records whether the program is currently in a word or not; iriitially it is "not in a word," which is assigned the value OUT. We prefer the symbolic constants IN and OUT to the literal values 1 and 0 because they make the program more readable. In a program as tiny as this, it makes little difference, but in larger programs, the increase in clarity is well worth the modest extra effort to write it this way from the beginning. You'll also find that it's easier to make extensive changes in programs where magic numbers appear only as symbolic constants. The line
