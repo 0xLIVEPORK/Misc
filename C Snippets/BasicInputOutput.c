d4f7bbbb9bb7194a2728e140b74f6b497632ee6f
EVILPORK <0xlivepork@gmail.com>
2025-05-09 20:49:59 +0800

    Update BasicInputOutput.c

--- C Snippets/BasicInputOutput.c
+++ C Snippets/BasicInputOutput.c
@@ -78,3 +78,36 @@ main()
     }
     printf("\n%d LINE NUMBER\n%d WORD NUMBER\n%d CHAR NUMBER", lineCount, wordCount, charCount);
 }
+
+
+////Exercise 1-10. Write a program to copy its input to its output, replacing each tab by \t, each backspace by \b, 
+//and each backslash by \ \. This makes tabs and backspaces visible in an unambiguous way. 
+#include <stdio.h>
+
+main() 
+{
+    int character;
+
+    while((character = getchar()) != EOF)
+    {
+        if(character == '\t')
+        {
+            putchar('\\');
+            putchar('t');
+        } else{
+            if(character == '\b')
+            {
+                putchar('\\');
+                putchar('b');
+            } else{
+                if(character == '\\')
+                {
+                    putchar('\\');
+                    putchar('\\');
+                } else{
+                    putchar(character);
+                }
+            }
+        }
+    }
+    
