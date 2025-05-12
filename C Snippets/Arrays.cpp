bdedbb3860d6f147818f2eef0c4b8781af7eaf4a
EVILPORK <0xlivepork@gmail.com>
2025-05-12 21:49:17 +0800

    Update Arrays.cpp

--- C Snippets/Arrays.cpp
+++ C Snippets/Arrays.cpp
@@ -1,5 +1,7 @@
 //Let us write a program to count the number of occurrences of each digit, of white space 
-//characters (blank, tab,f
+//characters (blank, tab, newline), and of all other characters. This is artificial, but 
+//it permits us to illustrate several aspects of C in one program.
+/*
 #include <stdio.h>
 
 main()
