eba7886578b1b820247fca4f072156059439eb3b
EVILPORK <0xlivepork@gmail.com>
2025-05-13 22:19:10 +0800

    Update FooFunct.c

--- C Snippets/FooFunct.c
+++ C Snippets/FooFunct.c
@@ -1,1 +1,20 @@
 //K&R pre C89
+
+#include <stdio.h>
+
+int Exponent(int base, int exponent)
+    {
+        int x = base;
+        for(exponent; exponent >= 0; exponent--)
+            base = base * x;
+            printf("%d", base); //testing
+            
+
+        return base;
+    }
+
+main() {
+    printf("TEST: %d\n", Exponent(2, 4));
+}
+
+
