6c9a1c2048112ce8adb2988c1a2d8b02fc37bd4d
EVILPORK <0xlivepork@gmail.com>
2025-05-14 21:33:00 +0800

    Update Histogram.c

--- C Snippets/Histogram.c
+++ C Snippets/Histogram.c
@@ -7,3 +7,42 @@ main()
 {
 
 }
+/*
+#include <stdio.h>
+
+int testfuct();
+
+main()
+{
+    printf("Test GCC -std=c89: %d", testfuct(10, 10));
+}
+
+testfuct(base, n)
+int base, n;
+{
+    int cock = base * n;
+
+    return cock;
+}
+
+*/
+
+
+//HISTOGRAM
+//K&R C - REMEMBER THESE COMPILER FLAGS
+//gcc -std=c89 [file] -o [filename]
+#include <stdio.h>
+
+int hstDisplay();
+
+main()
+{
+
+}
+
+hstDisplay(stkheight)
+int stkheight;
+    {
+        for(; stkheight >= 0 ; --stkheight)
+            printf("")
+    }
