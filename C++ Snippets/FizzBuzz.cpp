8dc921405aff03d4b05617b98da8c9764bf0d49b
EVILPORK <0xlivepork@gmail.com>
2025-05-21 21:10:58 +0800

    Update FizzBuzz.cpp

--- C++ Snippets/FizzBuzz.cpp
+++ C++ Snippets/FizzBuzz.cpp
@@ -1,2 +1,18 @@
-  sfsf
-    
+#include <iostream>
+#include <limits>
+#include <cstdlib>
+
+void Fizz(int number)
+    {
+        if(number / 3 >= 1)
+            printf("FizzBuzz\n");
+        else  
+            std::cout << number << std::endl;
+    }
+
+
+
+int main() {
+    Fizz(3);
+    Fizz(2);
+}
