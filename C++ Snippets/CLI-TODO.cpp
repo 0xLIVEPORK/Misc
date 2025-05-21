a19f411813987e0ff2ccffa6fed619566d1e40ab
EVILPORK <0xlivepork@gmail.com>
2025-05-21 22:28:26 +0800

    Update CLI-TODO.cpp

--- C++ Snippets/CLI-TODO.cpp
+++ C++ Snippets/CLI-TODO.cpp
@@ -1,1 +1,147 @@
-v
+
+#include <iostream>
+#include <limits>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+
+class App{
+    private:
+        std::string user;
+
+    public:
+
+        App() = delete; 
+        App(std::string name) : user(name) {}
+
+        void checkFile() {
+
+            std::ifstream chkFILE("user.txt");
+            std::string line;
+
+            if(!chkFILE)
+                {
+                    std::cerr << "File Does Not Exist";
+                    std::ofstream makeFILE("user.txt");
+                    if(!makeFILE)
+                        {
+                            std::cerr << "FILE CREATION ERROR";
+                        }
+                } 
+
+            else
+                {
+                    std::getline(chkFILE, line);
+                    std::cout << line;
+                }
+        }
+
+        void editList() {
+
+            std::function<void()> writetoFile = [](){
+                std::ofstream editFile("user.txt");
+
+                if(!editFile)
+                        std::cerr << "Error File Handling";
+
+
+                else
+            }
+        }
+
+
+        void Menu_Loop() {
+
+            int choice;
+
+            std::cout << "\t[TO DO]\t\n"
+                         "#  CHOOSE THE NUMBER\n\n"
+                         "[1] OPEN TO DO LIST\n"
+                         "[2] EDIT LIST\n"
+                         "[3] USER SETTINGS\n"
+                         "[4] LOG-OUT";
+
+            while(true)
+                {
+                if(!(std::cin >> choice) || choice < 1 || choice > 4)
+                    {
+                        std::cerr << "ERROR: INVALID INPUT";
+                        std::cin.clear();
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                        continue;
+                    }
+                break;
+                } 
+
+            switch(choice)
+            {
+                case 1:
+                checkFile();
+                break;
+
+                case 2:
+                std::cout << "Incomplete Feature";
+                break;
+
+                case 3:
+                set_USERNAME();
+                break;
+
+                case 4:
+                exit(0);
+                break;
+
+                default:
+                std::cout << "You're out of bounds lil bro";
+
+            }
+        }
+
+        void set_USERNAME() {
+            std::string new_name;
+            std::string confirm_name;
+        
+            while (true) {
+                std::cout << "Enter Your [NEW NAME]: ";
+                std::cin >> new_name;
+        
+                while (true) {
+                    std::cout << "CONFIRM [Y] or [N]? ";
+                    std::cin >> confirm_name;
+        
+                    if (confirm_name == "Y") {
+                        user = new_name;
+                        return;  
+                    }
+                    else if (confirm_name == "N") {
+                        std::cout << "Let's try again.\n";
+                        break; 
+                    }
+                    else {
+                        std::cerr << "Invalid input. Please enter Y or N.\n";
+
+                        std::cin.clear();
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    }
+                }
+            }
+        }
+        
+
+        std::string getUser() {
+            return user;
+        }
+
+
+};
+
+
+int main()
+{
+    App Todo("Cock");
+
+    Todo.Menu_Loop();
+
+
+    return 0;
+}
