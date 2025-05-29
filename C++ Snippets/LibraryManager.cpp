b8faf2457b9fbccfc26d1a141ef74d09c2d99f4e
EVILPORK <0xlivepork@gmail.com>
2025-05-29 22:36:43 +0800

    Update LibraryManager.cpp

--- C++ Snippets/LibraryManager.cpp
+++ C++ Snippets/LibraryManager.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <limits>
 
+
+class Library{
+    
+    public:
+
+    private:
+
+    protected:
+};
+
+class Student{
+
+};
+
+class Teacher: Student{
+
+};
+
+int main()
+{
+
+}
