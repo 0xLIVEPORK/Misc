5cfcd4f4818a5dfd34d85f501dc1eef886df6202
EVILPORK <0xlivepork@gmail.com>
2025-06-01 22:32:25 +0800

    Update LibraryManager.cpp

--- C++ Snippets/LibraryManager.cpp
+++ C++ Snippets/LibraryManager.cpp
@@ -1,27 +1,78 @@
 #include <iostream>
 #include <cstdlib>
 #include <limits>
+#include <fstream>
 
-void set_bookdata();
 struct book_DATA
 {
     std::string Title, 
                 Author, 
                 ISB, 
-                Genre, 
+                Genre,
                 Availability;
     
 };
 
-void set_bookdata(book_DATA & book)
-    {
-        std::string dataentry;
+void set_bookdata(book_DATA & book);
 
+void writetofile_bkdata(book_DATA & book);
+
+
+int main(){
+    book_DATA test;
+
+    set_bookdata(test);
+
+
+}
+
+void set_bookdata(book_DATA & book) {
+
+
+    //DATA VALIDATION LAMBDA
+    auto enter_data = [](const std::string & prompt) -> std::string {
+    std::string line;
         while(true)
+        {
+        std::cout << prompt;
+            if(!(std::getline(std::cin, line)) || line.empty())
             {
-                if(!(std::getline(std::cin, book.Author))
-                    {
-                        
-                    }
-            }
-    }
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                continue;        
+            } 
+        break;
+        }
+    return line;
+    };
+    //DATA VALIDATION LAMBDA
+
+    //WRITE TO BOOK FILE LAMBDA
+    std::ifstream bookfile("book_list.txt", std::ios::app);
+    if(!bookfile)
+        {
+            std::cerr << "File Making Filled";
+        }
+    //WRITE TO BOOK FILE LAMBDA
+
+    //MAIN FUNCTION SECTION
+    //v.01
+    
+    book.Title = enter_data("Enter Book Title: ");
+    std::cout << "Book Title Set to: " << book.Title << "\n\n";
+
+    book.Author = enter_data("Enter Author: ");
+    std::cout << "Author Set to: " << book.Author << "\n\n";
+
+    book.Genre = enter_data("Enter Genre: ");
+    std::cout << "Genre Set to " << book.Genre << "\n\n";
+
+    book.ISB = enter_data("Enter ISB: ");
+    std::cout << "ISB Set to " << book.ISB << "\n\n";
+
+    book.Availability = enter_data("Enter Availability: ");
+    std::cout << "Availability Set to: " << book.Availability << "\n\n";
+
+
+}
+
