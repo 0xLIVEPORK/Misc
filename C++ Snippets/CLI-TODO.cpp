78fcccebdd27e6a79345ee7ff7e3c8ac33847751
EVILPORK <0xlivepork@gmail.com>
2025-06-03 23:05:50 +0800

    Update CLI-TODO.cpp

--- C++ Snippets/CLI-TODO.cpp
+++ C++ Snippets/CLI-TODO.cpp
@@ -1,252 +1,81 @@
-ffffffaffsaffaffas
 #include <iostream>
-#include <limits>
 #include <cstdlib>
+#include <limits>
 #include <fstream>
-#include <functional>
 
-class App{
-    private:
-        std::string user;fsf
-
-    public:
-
-        App() = delete; 
-        App(std::string name) : user(name) {}
-
-        void checkFile() {
-
-            std::ifstream chkFILE("user.txt");
-            std::string line;
-
-            if(!chkFILE)
-                {
-                    std::cerr << "File Does Not Exist";
-                    std::ofstream makeFILE("user.txt");
-                    if(!makeFILE)
-                        {
-                            std::cerr << "FILE CREATION ERROR";
-                        }
-                } 
-
-            else
-                {
-                    std::getline(chkFILE, line);
-                    std::cout << line;
-                }
-        }
-
-        void editList() {
-
-            unsigned int choice;
-
-            /** LAMBDASd
-            * Better set this now
-            * No need to reuse these functions anywhere else anyways
-            * If we do need so we can just copy paste it but I doubt it
-            * User does not need to rewrite files with the Quit option does he?
-            **/
-
-            //ADD TASK LAMBDA
-            std::function<void()> ADDTASK = [](){
-
-                std::string addtask_name, addtask_deadline, addtask_description;
-                std::fstream editFile("user.txt");
-
-                if(!editFile)
-                    {
-                        std::cerr << "Error File Handling";
-                    }
-                else
-                    {
-                        while(true)
-                        {
-
-
-                            std::cin.clear();
-                            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-                            std::cout << "\nEnter Task Name: ";
-                            std::getline(std::cin, addtask_name);
-                            
-                            std::cout << "\nEnter Dead Line: ";
-                            std::getline(std::cin, addtask_deadline);
-
-                            std::cout << "\nEnter Description: ";
-                            std::getline(std::cin, addtask_description);
-
-                            std::cout << "\nEnter \"0\" if you want to save, anything else will discard your task";
-                            
-                            unsigned int choice;
-
-                            if(!(std::cin >> choice) || choice > 0)
-                                {
-                                    std::cin.clear();
-                                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                                    continue;
-                                }
-
-                            else
-                                {
-                                    break;
-                                    editFile << addtask_name << " | " << addtask_deadline << " | " << addtask_description << std::endl;
-                                }
-                        }
-
-                    }
-                };
-            
-            std::function<void()> EDITTASK = [](){
-
-                std::fstream editFile("user.txt", std::ios::in | std::ios::out | std::ios::app);
-                
-                if(!editFile.is_open())
-                    {
-                        std::cerr << "FILE ERROR";
-                        return;
-                    }
-
-                    std::string line;
-                    std::string allContent;
-
-                    while(std::getline(editFile, line))
-                        std::cout << line;
-                };
-                
-            std::function<void()> NAME = [](){
-
-            };
-            
-            choice = 0;
-            while(true)
-                {
-                std::cout << "Pick what you want to edit:\n"
-                             "[0] - EDIT TASK\n"
-                             "[1] - ADD TASK\n"
-                             "[2] - EXIT";
-                    if(!(std::cin >> choice))
-                        {
-                            std::cerr << "\nINVALID";
-                            std::cin.clear();
-                            std::cin.ignore();
-                            continue;
-                        } 
-
-                    switch(choice)
-                    {
-                        case 0:
-                        EDITTASK();
-                        break;
-
-                        case 1:
-                        ADDTASK();
-
-                        default:
-                        std::cout << "END";
-                        return;
-                        
-                    }
-                    
-                    
-                }
-            }
-
-
-
-        void Menu_Loop() {
-            while(true)
-            { 
-                int choice;
-
-                std::cout << "\t[TO DO]\t\n"
-                            "#  CHOOSE THE NUMBER\n\n"
-                            "[1] OPEN TO DO LIST\n"
-                            "[2] EDIT LIST\n"
-                            "[3] USER SETTINGS\n"
-                            "[4] LOG-OUT \n\n: ";
-
-                while(true)
-                    {
-                    if(!(std::cin >> choice) || choice < 1 || choice > 4)
-                        {
-                            std::cerr << "ERROR: INVALID INPUT: ";
-                            std::cin.clear();
-                            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                            continue;
-                        }
-                    break;
-                    } 
-
-                switch(choice)
-                {
-                    case 1:
-                    checkFile();
-                    break;
-
-                    case 2:
-                    editList();
-                    break;
-
-                    case 3:
-                    set_USERNAME();
-                    break;
-
-                    case 4:
-                    exit(0);
-                    break;
-
-                    default:
-                    std::cout << "You're out of bounds lil bro";
-
-                }
-            }
-        }
-
-        void set_USERNAME() {
-            std::string new_name;
-            std::string confirm_name;
-        
-            while (true) {
-                std::cout << "Enter Your [NEW NAME]: ";
-                std::cin >> new_name;
-        
-                while (true) {
-                    std::cout << "CONFIRM [Y] or [N]? ";
-                    std::cin >> confirm_name;
-        
-                    if (confirm_name == "Y") {
-                        user = new_name;
-                        return;  
-                    }
-                    else if (confirm_name == "N") {
-                        std::cout << "Let's try again.\n";
-                        break; 
-                    }
-                    else {
-                        std::cerr << "Invalid input. Please enter Y or N.\n";
-
-                        std::cin.clear();
-                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                    }
-                }
-            }
-        }
-        
-
-        std::string getUser() {
-            return user;
-        }
-
-
+struct book_DATA
+{
+    std::string Title, 
+                Author, 
+                ISB, 
+                Genre,
+                Availability;
+    
 };
 
+void set_bookdata(book_DATA & book);
 
-int main()
-{
-    App Todo("Cock");
-
-    Todo.Menu_Loop();
+void writetofile_bkdata(book_DATA & book);
 
 
-    return 0;
+int main(){
+    book_DATA test;
+
+    set_bookdata(test);
+
+
 }
+
+void set_bookdata(book_DATA & book) {
+
+
+    //DATA VALIDATION LAMBDA
+    auto enter_data = [](const std::string & prompt) -> std::string {
+    std::string line;
+        while(true)
+        {
+        std::cout << prompt;
+            if(!(std::getline(std::cin, line)) || line.empty())
+            {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                continue;        
+            } 
+        break;
+        }
+    return line;
+    };
+    //===========================================================================
+
+    //WRITE TO BOOK FILE LAMBDA
+    std::fstream bookfile("book_list.txt", std::ios::app);
+    if(!bookfile)
+        {
+            std::cerr << "File Making Failed";
+        }
+
+    bookfile << book.Author << "|"
+             << book.Availability << "|";
+    //=======================================================
+
+    //MAIN FUNCTION SECTION
+    //v.01
+    
+    book.Title = enter_data("Enter Book Title: ");
+    std::cout << "Book Title Set to: " << book.Title << "\n\n";
+
+    book.Author = enter_data("Enter Author: ");
+    std::cout << "Author Set to: " << book.Author << "\n\n";
+
+    book.Genre = enter_data("Enter Genre: ");
+    std::cout << "Genre Set to " << book.Genre << "\n\n";
+
+    book.ISB = enter_data("Enter ISB: ");
+    std::cout << "ISB Set to " << book.ISB << "\n\n";
+
+    book.Availability = enter_data("Enter Availability: ");
+    std::cout << "Availability Set to: " << book.Availability << "\n\n";
+
+
+}
+
