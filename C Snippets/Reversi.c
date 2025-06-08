dea5a9d1957976fad2d9fa1f303582a0c70f9e83
EVILPORK <0xlivepork@gmail.com>
2025-06-08 20:26:30 +0800

    Update Reversi.c

--- C Snippets/Reversi.c
+++ C Snippets/Reversi.c
@@ -1,2 +1,20 @@
+#include <stdio.h>
+#include <string.h>
 
-void reversi()
+void reversi(char word[]);
+
+int main(void) {
+    char test[10] = "balls";
+
+    reversi(test);
+}
+
+
+void reversi(char word[])
+    {
+    putchar('\n');
+        for(int i = 0; i <= strlen(word); i++)
+            {
+                putchar(word[strlen(word)- i]);
+            }
+    }
