1d098653d0d0cf56346110c0e35eca31b9c6cb96
EVILPORK <0xlivepork@gmail.com>
2025-06-09 22:44:47 +0800

    Update ex4.py

--- Python/ex4.py
+++ Python/ex4.py
@@ -1,1 +1,7 @@
 
+  #INPUT
+#The Formula Used for Temperature Conversion of Fahrenheit and Celsius is:
+#The conversion of the temperature from Fahrenheit to Celsius formula is C = (F − 32) × 5 ⁄ 9.
+#conversion of the temperature from Celsius to Fahrenheit formula is F = C(9 ⁄ 5) + 32.
+
+
