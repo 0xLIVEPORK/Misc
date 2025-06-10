73c0ee2ff5c51c2708dacac15afdd41c9415eba2
EVILPORK <0xlivepork@gmail.com>
2025-06-10 22:42:41 +0800

    Update ex6.py

--- Python/ex6.py
+++ Python/ex6.py
@@ -1,7 +1,7 @@
 from sys import argv
 script, first, second, third = argv
 
-print("The script say: ", )
-print("The script say: ", )
-print("The script say: ", )
-print("The script say: ", )
+print("The script say: ", script)
+print("The first say: ", first)
+print("The second say: ", second)
+print("The third say: ", third)
